9290544d278a76f332e3b58bcfde40ba1abf0304
Stephen Goeppele <s.goeppele.parrish@gmail.com>
2018-05-23 22:38:36 -0700

    Add functionality for sorting after moving bookmarks

--- automarks.js
+++ automarks.js
@@ -1,4 +1,9 @@
 /**
+ * Keeps tracks of which folders are actively being sorted.
+ */
+var active = new Set();
+
+/**
  * Compares a and b.
  *
  * @param a A BookmarkTreeNode object.
@@ -9,31 +14,40 @@ function comparator(a, b) {
   let foldersMode = 1;
   let field = "title";
   //let field = "url";
-  let reverse = false;
+  let isReversed = false;
+  let isCaseSensitive = false;
 
-  if (foldersMode) {
-    if (a.type === "folder" && b.type === "folder") {
-      if (a.title.toLowerCase() < b.title.toLowerCase()) { return -1; }
-      if (a.title.toLowerCase() > b.title.toLowerCase()) { return 1; }
-      return 0;
+  function compare(a, b) {
+    if (foldersMode) {
+      if (a.type === "folder" && b.type === "folder") {
+        let titleA = isCaseSensitive ? a.title : a.title.toLowerCase();
+        let titleB = isCaseSensitive ? b.title : b.title.toLowerCase();
+
+        if (titleA < titleB) { return -1; }
+        if (titleA > titleB) { return 1; }
+
+        return 0;
+      }
+      if (a.type === "folder") { return -foldersMode; }
+      if (b.type === "folder") { return foldersMode; }
     }
-    if (a.type === "folder") { return -foldersMode; }
-    if (b.type === "folder") { return foldersMode; }
-  }
 
-  let result = 0;
-  if (a[field].toLowerCase() < b[field].toLowerCase()) { result = -1; }
-  if (a[field].toLowerCase() > b[field].toLowerCase()) { result = 1; }
+    let fieldA = isCaseSensitive ? a[field] : a[field].toLowerCase();
+    let fieldB = isCaseSensitive ? b[field] : b[field].toLowerCase();
+
+    if (fieldA < fieldB) { return -1; }
+    if (fieldA > fieldB) { return 1; }
+  }
   
-  return result * (reverse ? -1 : 1);
+  return compare(a, b) * (isReversed ? -1 : 1);
 }
 
 /**
  * Sort the given entries.
  * 
- * @param {Array} entries Array of BookmarkTreeNode objects.
+ * @param {string} id String of the ID of the bookmark folder to sort. 
  */
-function autosort(entries) {
+function autosort(id) {
 
   function append(contents, section) {
     section.sort(comparator);
@@ -41,41 +55,63 @@ function autosort(entries) {
     section.length = 0;
   }
 
-  function organize(entries) {
+  function isSorted(array) {
+    for (let i = 1; i < array.length; i++) {
+      if (array[i - 1].type !== "separator"
+          && array[i].type !== "separator"
+          && comparator(array[i - 1], array[i]) > 0) {
+        return [array, false];
+      }
+    }
+    return [array, true];
+  }
+
+  function organize([entries, sorted]) {
     let contents = [];
     let section = [];
 
-    for (let entry of entries) {
-      if (entry.type === "separator") {
-        append(contents, section);
-        contents.push(entry);
-      } else {
-        section.push(entry);
+    if (!sorted) {
+      for (let entry of entries) {
+        if (entry.type === "separator") {
+          append(contents, section);
+          contents.push(entry);
+        } else {
+          section.push(entry);
+        }
       }
+      append(contents, section);
     }
 
-    append(contents, section);
-
-    return contents;
+    return [contents, sorted];
   }
 
-  console.log("begin autosorting");
-
-  let contents = organize(entries);
-  let sync = new Promise(resolve => { resolve(null); });
-  for (let i = 0; i < contents.length; i++) {
-    sync = sync.then(() => {
-      return browser.bookmarks.move(contents[i].id, {index: i})
-    }).then(resolve => {
-      // do nothing?
-      console.log(resolve);
-    }, reject => {
-      console.log("error");
-      console.log(reject);
-    });
+  function doMoving([contents, sorted]) {
+    let sync = Promise.resolve();
+    if (!sorted) {
+      for (let i = 0; i < contents.length; i++) {
+        sync = sync.then(() => {
+          return browser.bookmarks.move(contents[i].id, {index: i});
+        })
+        .then(bookmark => console.log(bookmark));
+      }
+    } else {
+      console.log("is already sorted");
+    }
+    return [sync];
   }
 
-  sync.then(() => console.log("done autosorting"));
+  if (!active.has(id)) {
+    active.add(id);
+    console.log(`starting:autosort(${id})`);
+    browser.bookmarks.getChildren(id)
+      .then(isSorted)
+      .then(organize)
+      .then(doMoving)
+      .then(([sync]) => sync.then(() => setTimeout(() => {
+        active.delete(id);
+        console.log(`finished:autosort(${id})`)
+      }, 3000)));
+  }
 }
 
 /**
@@ -83,48 +119,33 @@ function autosort(entries) {
  */
 function autosortAll() {
 
-  function recurse(nodes) {
-    nodes.filter(e => e.type === "folder")
-      .forEach(e => recurse(e.children));
-    autosort(nodes);
+  function recurse(node) {
+    if (node.type === "folder") {
+      node.children.forEach(recurse);
+      autosort(node.id);
+    }
   }
 
   browser.bookmarks.getSubTree("menu________")
-    .then(([menu]) => recurse(menu.children));
+    .then(([menu]) => recurse(menu));
 }
 
 // Events
 
 function handleCreated(id, bookmark) {
-  console.log("create triggered!");
-  browser.bookmarks.getChildren(bookmark.parentId).then(autosort);
+  autosort(bookmark.parentId);
 }
 
 function handleChanged(id, changeInfo) {
-  console.log("change triggered!");
-  browser.bookmarks.get(id)
-    .then(([bookmark]) => bookmark.parentId)
-    .then(browser.bookmarks.getChildren)
-    .then(autosort);
+  browser.bookmarks.get(id).then(([b]) => autosort(b.parentId));
 }
 
-function handleMovedInter(id, moveInfo) {
-  if (moveInfo.parentId !== moveInfo.oldParentId) {
-    console.log("cross-folder move triggered!");
-    browser.bookmarks.getChildren(moveInfo.parentId).then(autosort);
-  }
-}
-
-function handleMovedIntra(id, moveInfo) {
-  if (moveInfo.parentId === moveInfo.oldParentId) {
-    console.log("same-folder move triggered!");
-    browser.bookmarks.getChildren(moveInfo.parentId).then(autosort);
-  }
+function handleMoved(id, moveInfo) {
+  autosort(moveInfo.parentId);
 }
 
 autosortAll();
 
-browser.bookmarks.onMoved.addListener(handleMovedInter);
-//browser.bookmarks.onMoved.addListener(handleMovedIntra);
+browser.bookmarks.onMoved.addListener(handleMoved);
 browser.bookmarks.onCreated.addListener(handleCreated);
 browser.bookmarks.onChanged.addListener(handleChanged);
